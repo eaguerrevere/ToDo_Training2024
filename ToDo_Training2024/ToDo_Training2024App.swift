899b5e4f92f1778eb1f4c2a5e0d9de5b50d8a648
Eli <elizabeth.aguerrevere@gmail.com>
2024-05-05 15:55:33 -0700

    Initial Commit

---
+++ ToDo_Training2024/ToDo_Training2024App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_Training2024App.swift
+//  ToDo_Training2024
+//
+//  Created by Elizabeth Aguerrevere on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_Training2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
